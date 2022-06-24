908f4dff9db81d0431b34d3d7ef0598e61d47d56
Arkam97 <mahamed.arkam@itelasoft.com>
2022-06-24 14:51:15 +0530

    Listing screen and listingEdit added

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import React, { useContext, useState } from "react";
 import * as Yup from "yup";
 import FormField from "../components/forms/FormField";
 import Form from "../components/forms/Form";
@@ -7,9 +7,11 @@ import Screen from "../components/Screen";
 import SubmitButton from "../components/forms/SubmitButton";
 import FormPicker from "../components/forms/FormPicker";
 import FormImagePicker from "../components/forms/FormImagePicker";
-import UploadScreen from "./UploadScreen";
+// import UploadScreen from "./UploadScreen";
 import { addItem } from "../api/Item";
 import CategoryPickerItem from "../components/CategoryPickerItem";
+import { ScrollView } from "react-native-gesture-handler";
+import { AuthContext } from "../auth/AuthProvider";
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
@@ -76,66 +78,55 @@ const categories = [
   },
 ];
 
-const ListingEditScreen = () => {
-  const [uploadVisible, setUploadVisible] = useState(false);
-  const [progress, setProgress] = useState(0);
-
+const ListingEditScreen = ({ navigation }) => {
+  const { user } = useContext(AuthContext);
   const handleSubmit = async (listing, { resetForm }) => {
-    setProgress(0);
-    setUploadVisible(true);
     const result = await addItem({ ...listing });
-
-    if (!result.ok) {
-      setUploadVisible(false);
-      return alert("Could not save listing.");
-    }
-
     resetForm();
+    navigation.navigate("ListingScreen");
   };
   return (
-    <Screen style={styles.container}>
-      <UploadScreen
-        onDone={() => setUploadVisible(false)}
-        progress={progress}
-        visible={uploadVisible}
-      />
-      <Form
-        initialValues={{
-          title: "",
-          price: "",
-          description: "",
-          category: null,
-          images: [],
-        }}
-        onSubmit={handleSubmit}
-        validationSchema={validationSchema}
-      >
-        <FormImagePicker name="images" />
-        <FormField maxLength={255} name="titleMask" placeholder="Title" />
-        <FormField
-          keyboardType="numeric"
-          maxLength={8}
-          name="price"
-          placeholder="Price"
-        />
-        <FormPicker
-          items={categories}
-          name="category"
-          numberOfColumns={3}
-          PickerItemComponent={CategoryPickerItem}
-          placeholder="Category"
-          // width="50%"
-        />
-        <FormField
-          maxLength={255}
-          // multiline
-          name="description"
-          numberOfLines={4}
-          placeholder="Description"
-        />
-        <SubmitButton title="POST" alignItems="center" />
-      </Form>
-    </Screen>
+    <ScrollView>
+      <Screen style={styles.container}>
+        <Form
+          initialValues={{
+            user: user.uid,
+            title: "",
+            price: "",
+            description: "",
+            category: null,
+            images: [],
+          }}
+          onSubmit={handleSubmit}
+          validationSchema={validationSchema}
+        >
+          <FormImagePicker name="images" />
+          <FormField maxLength={255} name="title" placeholder="Title" />
+          <FormField
+            keyboardType="numeric"
+            maxLength={8}
+            name="price"
+            placeholder="Price"
+          />
+          <FormPicker
+            items={categories}
+            name="category"
+            numberOfColumns={3}
+            PickerItemComponent={CategoryPickerItem}
+            placeholder="Category"
+            // width="50%"
+          />
+          <FormField
+            maxLength={255}
+            // multiline
+            name="description"
+            numberOfLines={4}
+            placeholder="Description"
+          />
+          <SubmitButton title="POST" alignItems="center" />
+        </Form>
+      </Screen>
+    </ScrollView>
   );
 };
 
--- app/screens/ListingScreen.js
+++ app/screens/ListingScreen.js
@@ -1,11 +1,69 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { FlatList, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import useApi from "../hooks/useApi";
+import ActivityIndicator from "../components/ActivityIndicator";
+import HomeScreen from "../components/homeScreen";
+import AppText from "../components/Text";
+import Button from "../components/Button";
+import { getItems } from "../api/Item";
+import Card from "../components/Card";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import colors from "../config/colors";
 
-const ListingScreen = () => {
+const ActionBarImage = () => {
   return (
-    <View>
-      <Text>ListingScreen</Text>
+    <View style={{ flexDirection: "row" }}>
+      <Image
+        source={{
+          uri: "https://raw.githubusercontent.com/AboutReact/sampleresource/master/logosmalltransparen.png",
+        }}
+        style={{
+          width: 40,
+          height: 40,
+          borderRadius: 40 / 2,
+          marginLeft: 15,
+        }}
+      />
     </View>
+  );
+};
+const ListingScreen = ({ navigation }) => {
+  const {
+    data: listings,
+    error,
+    loading,
+    request: loadListings,
+  } = useApi(getItems);
+
+  useEffect(() => {
+    loadListings();
+  }, []);
+
+  return (
+    <>
+      <ActivityIndicator visible={loading} />
+      <HomeScreen style={styles.screen}>
+        {error && (
+          <>
+            <AppText>Couldn't retrieve the listings.</AppText>
+            <Button title="Retry" onPress={loadListings} />
+          </>
+        )}
+        <FlatList
+          data={listings}
+          keyExtractor={(listing) => listing.id.toString()}
+          numColumns={2}
+          renderItem={({ item }) => (
+            <Card
+              title={item.title}
+              subTitle={"$" + item.price}
+              imageUrl={item.images[0]}
+              onPress={() => navigation.navigate("ListingDetailScreen", item)}
+            />
+          )}
+        />
+      </HomeScreen>
+    </>
   );
 };
 
